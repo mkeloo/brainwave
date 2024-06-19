7c69b72f3072666310cf7bb416f13acd8577064e
mkeloo <mkeloo@ufl.edu>
2024-06-19 07:44:41 -0400

    onto header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import ButtonGradient from './assets/svg/ButtonGradient';
+import Button from './components/Button';
 
-function App() {
+const App = () => {
   return (
-    <main>
-      <h1 className="text-3xl font-bold">Hello World!</h1>
-    </main>
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10">Something</Button>
+      </div>
+      <ButtonGradient />
+    </>
   );
-}
+};
 
 export default App;
+
+// TIMESTAMP: Header Part
